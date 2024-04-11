5cf226b0563418754663c8a860347479c75929ca
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-04-11 11:06:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
+// Global Variables
+let player = {
+  x: 400,
+  y: 300,
+  w: 25,
+  h: 25,
+  speed: 5,
+  color: "blue",
+};
+
 // Draw Function
 window.addEventListener("load", draw);
 
@@ -16,6 +26,10 @@ function draw() {
   // Background
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  //   Draw Player
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.w, player.h);
 
   // Animation Loop
   requestAnimationFrame(draw);
