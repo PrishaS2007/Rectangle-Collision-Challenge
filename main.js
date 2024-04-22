1358194d52648a7546b3583762f0e455058d8f24
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-04-22 10:32:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -30,61 +30,61 @@ let rect1 = {
   h: 20,
 };
 
-// let rect2 = {
-//   x: 356,
-//   y: 200,
-//   w: 20,
-//   h: 150,
-// };
+let rect2 = {
+  x: 356,
+  y: 200,
+  w: 20,
+  h: 150,
+};
 
-// let rect3 = {
-//   x: 500,
-//   y: 100,
-//   w: 190,
-//   h: 20,
-// };
+let rect3 = {
+  x: 500,
+  y: 100,
+  w: 190,
+  h: 20,
+};
 
-// let rect4 = {
-//   x: 500,
-//   y: 450,
-//   w: 160,
-//   h: 120,
-// };
+let rect4 = {
+  x: 500,
+  y: 450,
+  w: 160,
+  h: 120,
+};
 
-// let rect5 = {
-//   x: 160,
-//   y: 500,
-//   w: 120,
-//   h: 20,
-// };
+let rect5 = {
+  x: 160,
+  y: 500,
+  w: 120,
+  h: 20,
+};
 
-// let rect6 = {
-//   x: 600,
-//   y: 180,
-//   w: 20,
-//   h: 150,
-// };
+let rect6 = {
+  x: 600,
+  y: 180,
+  w: 20,
+  h: 150,
+};
 
-// let rect7 = {
-//   x: 156,
-//   y: 250,
-//   w: 20,
-//   h: 150,
-// };
+let rect7 = {
+  x: 156,
+  y: 250,
+  w: 20,
+  h: 150,
+};
 
-// let rect8 = {
-//   x: 300,
-//   y: 50,
-//   w: 100,
-//   h: 20,
-// };
+let rect8 = {
+  x: 300,
+  y: 50,
+  w: 100,
+  h: 20,
+};
 
-// let rect9 = {
-//   x: 400,
-//   y: 460,
-//   w: 20,
-//   h: 150,
-// };
+let rect9 = {
+  x: 400,
+  y: 460,
+  w: 20,
+  h: 150,
+};
 
 let leftPressed = false;
 let rightPressed = false;
@@ -93,6 +93,32 @@ let downPressed = false;
 
 // Draw Function
 window.addEventListener("load", draw);
+
+function keydownHandler(e) {
+  if (e.code === "ArrowLeft") {
+    leftPressed = true;
+  } else if (e.code === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.code === "ArrowUp") {
+    upPressed = true;
+  } else if (e.code === "ArrowDown") {
+    downPressed = true;
+  }
+}
+
+document.addEventListener("keyup", keyupHandler);
+
+function keyupHandler(e) {
+  if (e.code === "ArrowLeft") {
+    leftPressed = false;
+  } else if (e.code === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.code === "ArrowUp") {
+    upPressed = false;
+  } else if (e.code === "ArrowDown") {
+    downPressed = false;
+  }
+}
 
 function draw() {
   // LOGIC
@@ -112,57 +138,71 @@ function draw() {
     document.body.style.backgroundColor = "lightgrey";
   } else {
     document.body.style.backgroundColor = "white";
-
-    // DRAWING
-    drawFrame();
-    // Animation Loop
-    requestAnimationFrame(draw);
   }
+  if (rectCollide(player, rect2)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect3)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect4)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect5)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect6)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect7)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect8)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  if (rectCollide(player, rect9)) {
+    document.body.style.backgroundColor = "lightgrey";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+
+  // DRAWING
+  drawFrame();
+  // Animation Loop
+  requestAnimationFrame(draw);
 
   document.addEventListener("keydown", keydownHandler);
+}
 
-  function keydownHandler(e) {
-    if (e.code === "ArrowLeft") {
-      leftPressed = true;
-    } else if (e.code === "ArrowRight") {
-      rightPressed = true;
-    } else if (e.code === "ArrowUp") {
-      upPressed = true;
-    } else if (e.code === "ArrowDown") {
-      downPressed = true;
-    }
-  }
+function drawFrame() {
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-  document.addEventListener("keyup", keyupHandler);
+  // Block
+  drawBlock(-1, 100, 150, 20);
+  drawBlock(356, 200, 20, 150);
+  drawBlock(500, 100, 190, 20);
+  drawBlock(500, 450, 160, 20);
+  drawBlock(160, 500, 120, 20);
+  drawBlock(600, 180, 20, 150);
+  drawBlock(156, 250, 20, 150);
+  drawBlock(300, 50, 100, 20);
+  drawBlock(400, 460, 20, 150);
 
-  function keyupHandler(e) {
-    if (e.code === "ArrowLeft") {
-      leftPressed = false;
-    } else if (e.code === "ArrowRight") {
-      rightPressed = false;
-    } else if (e.code === "ArrowUp") {
-      upPressed = false;
-    } else if (e.code === "ArrowDown") {
-      downPressed = false;
-    }
-  }
-
-  function drawFrame() {
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
-
-    // Block
-    drawBlock(-1, 100, 150, 20);
-    drawBlock(356, 200, 20, 150);
-    drawBlock(500, 100, 190, 20);
-    drawBlock(500, 450, 160, 20);
-    drawBlock(160, 500, 120, 20);
-    drawBlock(600, 180, 20, 150);
-    drawBlock(156, 250, 20, 150);
-    drawBlock(300, 50, 100, 20);
-    drawBlock(400, 460, 20, 150);
-
-    //   Draw Player
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.x, player.y, player.w, player.h);
-  }
+  //   Draw Player
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.w, player.h);
 }
